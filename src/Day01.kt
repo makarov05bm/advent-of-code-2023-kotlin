e4b8a4acc4946f92ec0563ddcf5f43c3750545d9
makarov <77200870+makarov05bm@users.noreply.github.com>
2023-12-01 18:51:03 +0100

    Update Day01.kt

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,26 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+import java.io.File
+
+fun main(args: Array<String>) {
+    val linesList = mutableListOf<String>()
+
+    File("src/data.txt").useLines { lines -> lines.forEach { item -> linesList.add(item) } }
+
+    val finalList = mutableListOf<Int>()
+
+    for (line in linesList) {
+        var strDigit = ""
+        val digitsList = mutableListOf<Char>()
+
+        for (char in line) {
+            if (char.code in 48..57) {
+                digitsList.add(char)
+            }
+        }
+
+        strDigit = digitsList[0].toString() + digitsList[digitsList.size - 1].toString()
+
+        finalList.add(strDigit.toInt())
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
-    part1(input).println()
-    part2(input).println()
+    println("Sum: ${finalList.sum()}")
 }
